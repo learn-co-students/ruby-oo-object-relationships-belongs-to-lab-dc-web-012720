b0f70b33f4d18572269a594a7b503478ab85fb86
bumbot <pkim1295@gmail.com>
2020-01-30 08:35:39 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song
+    attr_accessor :title
+    attr_reader :artist
+
+    def initialize()
+        @title = title
+    end
+
+    def artist=(artist)
+        @artist = artist
+    end
+end
