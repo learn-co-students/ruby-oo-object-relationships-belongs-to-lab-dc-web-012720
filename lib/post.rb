54b4253e732c94ece3a93bae7f7bbc45f5881814
matteoaricci <matteo.a.ricci@gmail.com>
2020-01-29 20:53:15 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,10 @@
+class Post
+    attr_accessor :title, :author
+    def initialize
+        @title
+    end
+
+    def author=(author_name)
+        @author = author_name
+    end
+end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+    attr_accessor :title, :artist
+
+    def initialize
+        @title
+    end
+
+    def artist=(artist_name)
+        @artist = artist_name
+    end
+end
